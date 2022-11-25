c21432c6592d0237bc82560bb848b965a812ce2e
PrzybylowskiBartosz <bartosz@mytimeplan.com>
2022-11-25 10:59:26 +0100

    implement tests

--- src/test/java/SearchEngineTest.java
+++ src/test/java/SearchEngineTest.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class SearchEngineTest {
+import com.findwise.SearchEngine;
+import com.findwise.SearchEngineImpl;
+import org.junit.Test;
+
+public class SearchEngineTest {
+
+    SearchEngine searchEngine = SearchEngineImpl.getInstance();
+
+    @Test
+    public void searchEngineTest() {
+        searchEngine.indexDocument("document1", "the brown fox jumped over the brown dog");
+    }
 }
