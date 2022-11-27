193189a6b1031ee4d5f8f0825fbb71d0d465c19f
PrzybylowskiBartosz <przybylowski.dev@gmail.com>
2022-11-27 16:03:32 +0100

    remove invalid methods from SearchEngineImpl

--- src/main/java/com/findwise/SearchEngineImpl.java
+++ src/main/java/com/findwise/SearchEngineImpl.java
@@ -1,17 +1,17 @@
 package com.findwise;
 
+import com.findwise.storage.DocumentStorage;
+import com.findwise.storage.DocumentStorageImpl;
 import com.findwise.storage.IndexEntryStorage;
 import com.findwise.storage.IndexEntryStorageImpl;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 public class SearchEngineImpl implements SearchEngine {
 
     private final IndexEntryStorage indexEntryStorage;
+    private final DocumentStorage documentStorage;
     private final TFIDFCalculator tfidfCalculator;
-    private final Tokenizer tokenizer;
 
     private static class Singleton {
         private static final SearchEngine INSTANCE = new SearchEngineImpl();
@@ -19,7 +19,7 @@ public class SearchEngineImpl implements SearchEngine {
 
     private SearchEngineImpl() {
         this.indexEntryStorage = IndexEntryStorageImpl.getInstance();
-        this.tokenizer = TokenizerImpl.getInstance();
+        this.documentStorage = DocumentStorageImpl.getInstance();
         this.tfidfCalculator = TFIDFCalculatorImpl.getInstance();
     }
 
@@ -29,38 +29,11 @@ public class SearchEngineImpl implements SearchEngine {
 
     @Override
     public void indexDocument(String id, String content) {
-        tokenizeDocument(id, content);
+        documentStorage.addDocument(id, content);
+
     }
 
-    //todo
-    private void tokenizeDocument(String documentId, String content) {
-        List<String> tokens = tokenizer.getTokens(content);
 
-        for (String token : tokens) {
-            Optional<IndexEntry> indexEntry = indexEntryStorage.getIndexEntryById(documentId, token);
-            if (indexEntry.isPresent()) {
-                updateIndexEntry(documentId, token, indexEntry.get());
-            } else {
-                createNewIndexEntry(documentId, token);
-            }
-        }
-    }
-
-    //todo
-    private void updateIndexEntry(String documentId, String token, IndexEntry indexEntry) {
-        double newScore = tfidfCalculator.getScore(token, documentId);
-        indexEntry.setScore(newScore);
-        indexEntryStorage.updateIndexEntry(documentId, indexEntry);
-    }
-
-    //todo
-    private void createNewIndexEntry(String documentId, String token) {
-        double scores = tfidfCalculator.getScore(token, documentId);
-        List<IndexEntry> indexEntry = Collections.singletonList(new IndexEntryImpl(documentId, scores));
-        indexEntryStorage.addIndexEntries(documentId, indexEntry);
-    }
-
-    //todo
     @Override
     public List<IndexEntry> search(String term) {
         indexEntryStorage.getDocumentsWithToken(term);
